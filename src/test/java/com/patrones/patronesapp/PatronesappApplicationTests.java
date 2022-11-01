bc1d042184d54449d9abd9e70573bf7a83779260
Diego Sánchez <diegosandmg@gmail.com>
2022-11-01 17:13:09 -0400

    feat: spring boot initial setup

---
+++ src/test/java/com/patrones/patronesapp/PatronesappApplicationTests.java
@@ -0,0 +1,13 @@
+package com.patrones.patronesapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PatronesappApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
